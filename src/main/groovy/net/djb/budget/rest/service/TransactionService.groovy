e3103bc326de189c7ad1bb0f9cca7b3d62444fc8
Dave Burke <dburke84@gmail.com>
2015-10-12 15:59:14 -0500

    Add TransactionService

---
+++ src/main/groovy/net/djb/budget/rest/service/TransactionService.groovy
@@ -0,0 +1,46 @@
+package net.djb.budget.rest.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Sort;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import net.djb.budget.rest.data.schema.Transaction;
+import net.djb.budget.rest.data.repo.TransactionRepository;
+
+@Service
+class TransactionService {
+
+	static final Logger LOG = LoggerFactory.getLogger(TransactionService.class);
+
+	static final int DEFAULT_PAGE_SIZE = 100;
+
+	@Autowired TransactionRepository transactions;
+
+	List<Transaction> list() {
+		return list(0);
+	}
+
+	List<Transaction> list(int pageNumber) {
+		Sort sort = new Sort(Sort.Direction.DESC, "time");
+		PageRequest pageRequest = new PageRequest(pageNumber, DEFAULT_PAGE_SIZE, sort);
+		Page<Transaction> page = transactions.findAll(pageRequest);
+		return page.content;
+	}
+
+	Transaction save(Transaction transaction) {
+		transactions.save(transaction);
+		LOG.info("Saved {}", transaction.description);
+		return transaction;
+	}
+
+	void delete(long transactionId) {
+		transactions.delete(transactionId);
+		LOG.info("Deleted {}", transactionId);
+	}
+
+}
