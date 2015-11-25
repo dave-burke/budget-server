dd08a812f5bbce077a0ee8c51442ad9a170d6cb0
Dave Burke <dburke84@gmail.com>
2015-11-25 06:53:44 -0600

    Fix test account path separator

--- src/test/java/net/djb/budget/rest/ServiceApplicationTests.java
+++ src/test/java/net/djb/budget/rest/ServiceApplicationTests.java
@@ -33,8 +33,8 @@ public class ServiceApplicationTests {
 		});
 
 		assertEquals(new Long(0L), balances.calcBalance(LocalDateTime.now()));
-		assertEquals(new Long(75L), balances.calcBalance("assets/checking", LocalDateTime.now()));
-		assertEquals(5, balances.calcBalances("assets/checking", LocalDateTime.now()).size());
+		assertEquals(new Long(75L), balances.calcBalance("assets:checking", LocalDateTime.now()));
+		assertEquals(5, balances.calcBalances("assets:checking", LocalDateTime.now()).size());
 	}
 
 }
