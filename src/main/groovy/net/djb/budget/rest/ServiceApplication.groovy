9bee2f37439b4d04c5cef41124873f1ec288fb9f
Dave Burke <dburke84@gmail.com>
2015-11-25 07:04:48 -0600

    Serialize java.time classes as ISO timestamps

--- src/main/groovy/net/djb/budget/rest/ServiceApplication.groovy
+++ src/main/groovy/net/djb/budget/rest/ServiceApplication.groovy
@@ -1,13 +1,25 @@
 package net.djb.budget.rest;
 
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 class ServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ServiceApplication.class, args);
+	}
+
+	@Bean
+	public ObjectMapper objectMapper(){
+		ObjectMapper objectMapper = new ObjectMapper();
+		objectMapper.registerModule(new JavaTimeModule());
+		objectMapper.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS , false);
+		return objectMapper;
 	}
 
 }
