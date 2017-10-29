68650f60361e0194afac5b3e83754d4a3ea69368
Dave Burke <dburke84@gmail.com>
2017-10-28 22:17:03 -0500

    Allow cross-origin requests
    At this time, the application will only be running on my local server.
    If I ever run it publicly with authentication, then this could be locked
    down, but even so HTTPS+Basic Auth should cover it.

--- src/main/groovy/net/djb/budget/rest/ServiceApplication.groovy
+++ src/main/groovy/net/djb/budget/rest/ServiceApplication.groovy
@@ -6,6 +6,9 @@ import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 
 @SpringBootApplication
 class ServiceApplication {
@@ -22,5 +25,16 @@ class ServiceApplication {
 		return objectMapper;
 	}
 
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurerAdapter() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+				.allowedMethods("GET", "PUT", "POST", "DELETE")
+				.allowedOrigins("*");
+			}
+		};
+	}
 }
 
